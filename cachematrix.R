5dc0a7e08c35e2aa9d24d653bb1f1a18e5d2ac10
Ewa <epoloczek@wp.pl>
2014-05-24 13:35:50 +0200

    commit 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+##Following functions help not to solve inverse of matrix every time there is need to use it, but only once.
+##(So running it first time takes the same as solving it in a "standard" way, every next time we spare time)
+
+
+##Function that return list of functions. In function getinv inverse matrix is being kept, so there is no need to solve it again and again (e.g. in case of copying)
 
 makeCacachecheMatrix <- function(m = numeric()) {
          inv <- NULL
@@ -14,6 +19,7 @@ makeCacachecheMatrix <- function(m = numeric()) {
               getinv = getinv)
  }
 
+## Function that check if the inverse matrix has been already solved. If it has been than function returns existing solution, in other case function solves it. 
 
 cacheSolve <- function(mList, ...) {
         inv <- mList$getinv()
